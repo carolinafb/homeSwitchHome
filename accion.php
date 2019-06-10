fae3b9b5fc1e3a9429ac8fdd67a9e0740c973111
flor <flomic08@gmail.com>
2019-06-10 15:39:38 -0300

    finalizado la hu registrarse

--- accion.php
+++ accion.php
@@ -3,35 +3,45 @@
        $apellido = $_POST["apellido"];
        $fechaNacimiento = $_POST["fechaNacimiento"];
        $email = $_POST["email"];
-       $contrasena = $_POST["contrasena"];  
-       $numerotarjeta = $_POST["numerotarjeta"];
+       $contrasena = $_POST["contrasena"];
        $pais = $_POST["pais"];
-	   $ok = true;
+       $numerotarjeta = $_POST["numerotarjeta"];
+       $nomYape = $_POST["nomYape"];
+       $expiracion= $_POST["expiracion"];
+       $codSeg = $_POST["codSeg"];
+	     $ok = true;
        $mMay= false;/*variable para el match con mayusculas VALIDACION DE CONTRASENA*/
-	   $mMin= false;/*variable para el match con minusculas*/
-	   $mSim= false;/*variable para el match con simbolo*/
+	     $mMin= false;/*variable para el match con minusculas*/
+	     $mSim= false;/*variable para el match con simbolo*/
 
-        if ( (empty(trim($nombre))) or (empty(trim($apellido))) or (empty(trim($fechaNacimiento))) or (empty(trim($numerotarjeta))) or (empty(trim($pais))) ) { /*me fijo ningun campo sea vacio que no es vacio*/
+        if ( (empty(trim($nombre))) or (empty(trim($apellido))) or (empty(trim($fechaNacimiento))) or (empty(trim($pais))) or (empty(trim($numerotarjeta))) or  (empty(trim($nomYape))) or (empty(trim($expiracion))) or  (empty(trim($codSeg))) ) { /*me fijo ningun campo sea vacio que no es vacio*/
 
 			$ok=false;
-			
+
 			}
-		if 	(empty(trim($email))) { // si el mail es esta vacio
+		$edad = date("Y-m-d",strtotime($fechaNacimiento."+ 18 years"));// fecha de nacimiento mas 18 años
+		$hoy = date("Y-m-d"); //fecha ahora
+
+		if(!($edad <= $hoy)){ //si la edad es menor o igual al dia de hoy
+		   	$ok = false;
+		    echo '<script>  alert ("Debes ser mayor de 18 años para poder registrarte");</script>';;
+		}
+		if 	(empty(trim($email))) { // si el mail no es esta vacio
 			$ok=false;
 			}
 			// si el mail no es vacio
-		 	else if (!preg_match("/^[A-z0-9\\._-]+@[A-z0-9][A-z0-9-]*(\\.[A-z0-9_-]+)*\\.([A-z]{2,6})$/",$email)) { /*SE VALIDA QUE EL MAIL TENGA EL FORMATO DE MAIL--> combinacion de letras/numero/signos seguidos de @ seguido de convinacion + . algo */				
+		 	else if (!preg_match("/^[A-z0-9\\._-]+@[A-z0-9][A-z0-9-]*(\\.[A-z0-9_-]+)*\\.([A-z]{2,6})$/",$email)) { /*SE VALIDA QUE EL MAIL TENGA EL FORMATO DE MAIL--> combinacion de letras/numero/signos seguidos de @ seguido de convinacion + . algo */
 				$ok=false;
 				echo '<script>  alert ("email incorrecto");</script>';
 			}
-		
+
 		if (empty(trim($contrasena))) { /*me fijo que no es vacio*/
 			$ok=false;
-			}	
+			}
 			else{
 				if (strlen($contrasena)>=6){/* si la contraseña tinene 6 o mas digitos */
-				    for($i=0;$i<strlen($contrasena);$i++){ 
-        		         if ((preg_match("/^([a-z])+$/",$contrasena[$i]))){ // por lo menos 1 mininuscula 
+				    for($i=0;$i<strlen($contrasena);$i++){
+        		         if ((preg_match("/^([a-z])+$/",$contrasena[$i]))){ // por lo menos 1 mininuscula
 		        		    $mMin= true;
 	              			}
 	            		 if (preg_match("/^([A-Z])+$/",$contrasena[$i])){ // por lo menos 1 mayuscula
@@ -49,7 +59,7 @@
 		    			echo '<script>  alert ("Contraseña incorrecta, faltan por lo menos 1 mayuscula  ");</script>';
 		    			$ok=false;
 		    		}
-		    		if (!$mSim){ 
+		    		if (!$mSim){
 						echo '<script>  alert ("Contraseña incorrecta, falta por lo menos 1 simbolo o numero");</script>';
 						$ok=false;
 					}
@@ -58,23 +68,23 @@
 					echo '<script>  alert ("La contraseña debe contener minimo 6 digitos ");</script>';
 
 				}
-	
+
 			}
-		
-		if ($ok===true){//OK ES VERDADERO PORQUE TODOS LOS DATOS SON LOS CORRECTOS 
+
+		if ($ok===true){//OK ES VERDADERO PORQUE TODOS LOS DATOS SON LOS CORRECTOS
 			include "conexion.php";
        		$link=conexion();
-       		$aux=mysqli_query($link,"SELECT * FROM usuario WHERE email = '$email'");// me fijo si existe el mail 
-      		if(mysqli_num_rows($aux) == 0){ // obtengo el numero de filas de la variable, si es 0 es porque el mail no esta registrado. 
-				mysqli_query ($link,"INSERT INTO `usuario`(`nombre`, `apellido`, `email`,`contrasena`,`pais`,`numeroTarjeta`,`fechaNacimiento`,`rol`) VALUES('$nombre','$apellido','$email','$contrasena','$pais','$numerotarjeta','$fechaNacimiento','ESTANDAR')");// FUNCIONA WOJU
+       		$aux=mysqli_query($link,"SELECT * FROM usuario WHERE email = '$email'");// me fijo si existe el mail
+      		if(mysqli_num_rows($aux) == 0){ // obtengo el numero de filas de la variable, si es 0 es porque el mail no esta registrado.
+				mysqli_query ($link,"INSERT INTO `usuario`(`nombre`, `apellido`, `email`,`contrasena`,`pais`,`fechaNacimiento`,`rol`,`numeroTarjeta`, `nombreYapellidoDeTarjeta`, `fechaExpiracion`, `codigoSeguridad`) VALUES('$nombre','$apellido','$email','$contrasena','$pais','$fechaNacimiento','ESTANDAR','$numerotarjeta','$nomYape','$expiracion','$codSeg')");// FUNCIONA WOJU
 				echo '<script> alert ("Usuario registrado correctamente");</script>';
 				echo '<script> window.location="index.php"; </script>';
-			}else{ 
+			}else{
          		echo '<script> alert ("El usuario que intenta registrar ya existe");</script>';
-      		  	echo '<script> window.location="index.php"; </script>';  
-     		} 
+      		  	echo '<script> window.location="index.php"; </script>';
+     		}
  		}else{
   			echo '<script> window.location="formularioderegistro.php";</script>';
-  			
+
   			}
 ?>
--- formularioderegistro.php
+++ formularioderegistro.php
@@ -9,7 +9,6 @@
   
   </head>
 <body>
-  
  <form name='datosUsuario' action="accion.php" method="POST" >
    <fieldset>
     <legend> <h1> Registro de Usuario </h1>	</legend>
@@ -18,43 +17,76 @@
           <td> Nombre: </td>
           <td><input type= 'text' name='nombre' id='nombre' required/></td>
         </tr>
-
     <th></th>    
        <tr>
           <td>Apellido: </td> 
           <td><input type= 'text' name='apellido' id='apellido' required/></td>
        </tr>
-
     <th></th>
       <tr>
         <td>Fecha Nacimiento: </td>
         <td> <input type="date" name="fechaNacimiento"> </td>      
       </tr>
-
     <th></th>
         <tr>
           <td> Email: </td>
           <td><input type= 'text' name='email' id='email' required/></td>
         </tr>
-    
     <th></th>
-    
-    <tr>
-      <td>Contrseña: </td>
-      <td><input type= 'password' name='contrasena' id='contrasena' title="Minimo 6 caracteres, letras mayusculas y minisculas y por lo menos un numero o simbolo" required/ ></td>
-       </tr>
-
+        <tr>
+         <td>Contrseña: </td>
+          <td><input type= 'password' name='contrasena' id='contrasena' title="Minimo 6 caracteres, letras mayusculas y minisculas y por lo menos un numero o simbolo" required/ ></td>
+        </tr>
     <th></th>    
+          <tr>
+            <td>Pais: </td> 
+            <td>
+              <select name='pais' id='pais' required>
+                <option>Argentina</option>
+                <option>Bolivia</option>
+                <option>Brasil</option>
+                <option>Chile</option>
+                <option>Colombia</option> 
+                <option>Costa Rica</option>
+                <option>Cuba</option>
+                <option>Ecuador</option>
+                <option>El Salvador</option>
+                <option>Guatemala</option>
+                <option>Honduras</option>
+                <option>México</option>
+                <option>Nicaragua</option>
+                <option>Panamá</option>
+                <option>Paraguay</option>
+                <option>Puerto Rico</option>
+                <option>Perú</option>
+                <option>República Dominicana</option>
+                <option>Uruguay</option>
+                <option>Venezuela</option>
+              </select>
+           <!--   <input type= 'text' name='pais' id='pais' required/>-->
+           </td>
+       </tr>
+       <th></th>
+       <tr>
+          <td><h2><strong> Datos de tarjeta</strong></h2></td>
+       </tr>
        <tr>
           <td>Numero de tarjeta: </td> 
           <td><input type= 'number' name='numerotarjeta' id='numerotarjeta' required/></td>
        </tr>
        <th></th>
-    
        <tr>
-          <td>Pais: </td> 
-          <td><input type= 'text' name='pais' id='pais' required/></td>
-       </tr>
+          <td>Nombre y apellido </td>
+          <td><input type="text" name="nomYape" id="nomYape" title="tal cual este impreso en la tarjeta" required/></td>
+       </tr> 
+       <tr>
+         <td>Fecha de expiracion</td>
+         <td><input type="Month" name="expiracion" id="expiracion"  required/></td>
+       </tr>  
+       <tr>
+         <td>Codigo de seguridad</td>
+         <td><input type="number" name="codSeg" id="codSeg"  required/></td>
+       </tr>  
   </table>
   <br>
   <input  type="submit" value="Registrarse" onclick="validarform()"> 
