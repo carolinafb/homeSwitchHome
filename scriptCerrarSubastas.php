627471dcd252a50117cf60342bad053c9f30f9b0
carolina fb <carolinafernandezbeltran@gmail.com>
2019-06-09 23:47:55 -0300

    Script para cerrar subasntas andando correctamente,falta info de usu y subasta ganadora

--- scriptCerrarSubastas.php
+++ scriptCerrarSubastas.php
@@ -5,38 +5,39 @@ include 'conexion.php';
 $link = conexion();
 if (isset($_SESSION['login'])) { // si la sesion esta iniciada muestra codigo, rol y el cerrar sesion
 	if ($_SESSION['rol'] == 'ADMINISTRADOR') {
-		$queryDisponibles = "SELECT * FROM subastas WHERE estado = 'DISPONIBLE' ";
-		$consultaDisponibles = mysqli_query($link, $queryDisponibles); // me traigo todas las subastas disponibles 
+		// me traigo todas las subastas disponibles 
+		$querySubastasDisponibles = "SELECT * FROM subastas WHERE estado = 'DISPONIBLE' ";
+		$consultaSubastasDisponibles = mysqli_query($link, $querySubastasDisponibles); 
 		//por cada subasta disponible controlo si pasaron los 3 dias
-		//si se cerro, llamo al cerrar subasta pasandole el id de la subasta
-		while ($fila = mysqli_fetch_array($consultaDisponibles)) { //Mientras existan filas en la consulta Fetch array: devuelve un array datos de la fila recuperada, o FALSE si no hay más filas. trae una fila y avanza el puntero -->
+		while ($fila = mysqli_fetch_array($consultaSubastasDisponibles)) { 
+		
 			$semanaFin = date("Y-m-d", strtotime(date("Y-m-d") . "-3 days")); // me creo estas variables para restringir que las semana de las subastas a crear sean dentro de la ventana permitida
 			if ($fila["fechaInicio"] <  $semanaFin) {
 				$idSub = $fila["ID"];
-				//Calcular el ganador 	
-				$queryGanador = "SELECT ID_Usuario, MAX(monto) FROM pujas WHERE ID_subasta=$idSub "; //El monto maximo pujado de la subasta. 
-				$consultaGanador = mysqli_query($link, $queryGanador);
-				if (mysqli_num_rows($consultaGanador) > 0) { // Si traigo filas
-					$fila = mysqli_fetch_array($consultaGanador);
+				//Calcular el usuario ganador  de la subasta pidiendo el monto maximo pujado de la subasta. 
+				$queryUsuarioGanador = "SELECT ID_Usuario, monto FROM pujas WHERE ID_subasta=$idSub ORDER BY monto DESC LIMIT 1  "; 
+				$consultaUsuarioGanador = mysqli_query($link, $queryUsuarioGanador);
+				// Si traigo un usuario ganador
+				if (mysqli_num_rows($consultaUsuarioGanador) > 0) { 
+					$fila = mysqli_fetch_array($consultaUsuarioGanador);
 					$IDUsu = $fila['ID_Usuario'];
-					$queryUsuario = "SELECT nombre,apellido FROM usuario WHERE ID=$IDUsu "; //El monto maximo pujado de la subasta. 
-					$consultaUsuario = mysqli_query($link, $queryUsuario);
-					$fila2 = mysqli_fetch_array($consultaUsuario);
-					// guardo el ID del Usuario ganador de la subasta el la tabla subasta
+					echo '<script>alert("Subasta cerrada correctamente, con ganador ") </script>';
+					//actualizo el ganador de la suasta
 					$query3 = "UPDATE subastas SET ID_UsuarioGanador = $IDUsu WHERE ID = $idSub";
-					// informo los datos del ganador en pantalla.
-					echo '<script> alert ("El ganador de la subasta es el usuario:<?php echo $fila2["nombre"] ,$fila2["apellido"];?>")</script>';
-					// actualizo el estado de la subasta a no disponible
-					$query = "UPDATE subastas SET estado = 'NO DISPONIBLE' WHERE ID = $idSub";
-					$consulta = mysqli_query($link, $query);
-					echo '<script>alert("Subasta cerrada correctamente")</script>';
+					$consultaquery3 = mysqli_query($link, $query3);								
+					
 				} else {
-					echo '<script>alert("Subasta cerrada correctamente, Ninguna puja.Sin ganador")</script>';
+					echo '<script>alert("Subasta cerrada correctamente, Sin puja.Sin ganador")</script>';
 				}
+				// actualizo el estado de la subasta a no disponible
+				$query = "UPDATE subastas SET estado = 'NO DISPONIBLE' WHERE ID = $idSub";
+				$consulta = mysqli_query($link, $query);
 			}
 		}
 	}
 } ?>
-<script> window.location="listarSubastas.php"</script>
+<!--<script>
+	window.location = "listarSubastas.php"
+</script>-->
 
 </html>
