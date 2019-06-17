077e6b4a62384b7900c605cc7919613c1eb0a541
CamiLG <garciacamilalorena@gmail.com>
2019-06-17 10:55:44 -0300

    Alta propiedad: form y alta en base de datosFUNCIONA

--- ingresarPropiedad.php
+++ ingresarPropiedad.php
@@ -10,17 +10,16 @@
       $pais= $_POST['paisPropiedad'];
       $provincia= $_POST['provinciaPropiedad'];
       $ciudad = $_POST['ciudadPropiedad'];
-      // $estado= $_POST['estadoPropiedad'];
+      $estado= 'DISPONIBLE';
       $foto = $_POST ['fotoPropiedad'];
       $idAdmin = $_SESSION['email'];
 
-      $fecha=date('Y-m-d');
  
-      $query="INSERT INTO propiedades ( nombre, descripcion, precio, direccion, pais, provincia, ciudad, foto, ID_administrador, semanaDeAlta,semanaInicioVentana, semanafinVentana, anioInicioVentana, anioFinVentana )VALUES ('$nombre', '$descripcion', '$precio', '$direccion', '$pais', '$provincia','$ciudad','$foto','$idAdmin', '$fecha', DATE_ADD(CURDATE(), INTERVAL 6 MONTH),DATE_ADD(CURDATE(), INTERVAL 1 YEAR))";
-      mysqli_query($link,$query);
+      $query="INSERT INTO `propiedades` ( nombre, descripcion, precio, direccion, pais, provincia, ciudad, estado, foto, ID_administrador, semanaDeAlta,semanaInicioVentana, semanafinVentana, anioInicioVentana, anioFinVentana )VALUES ('$nombre', '$descripcion', '$precio', '$direccion', '$pais', '$provincia','$ciudad','$estado', '$foto','$idAdmin', 'CURDATE()', 'DATE_ADD(CURDATE(), INTERVAL 6 MONTH)' , 'DATE_ADD(CURDATE(), INTERVAL 1 YEAR)', 'DATE_ADD(CURDATE(), INTERVAL 6 MONTH)', 'DATE_ADD(CURDATE(), INTERVAL 1 YEAR)')";
+      $resul= mysqli_query($link,$query);
+
       echo '<script> alert ("Propiedad cargada correctamente")</script>';
       echo '<script> window.location="index.php"</script>'; // Esto deberia ir al listado de propiedades
-
 //en date W me da el numero de semanas
 // este anda CURDATE() es para mandar la fecha directamente en la consultaa
 ?>
