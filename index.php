ee4863990c031b58f4ffc86c3113d97066050388
flor <flomic08@gmail.com>
2019-06-17 11:12:04 -0300

    cambios de flor

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
 	<?php }?>
 
 	<center> <!-- esta es la imagen de la portada -->
-			<div class='portada'><img src='imagenes/logoHomeSwitchHome.png' title="Home Switch Home" width="550" height="250" > </div>
+			<div class='portada'><a href='index.php'><img  src='imagenes/logoHomeSwitchHome.png' title="Home Switch Home" width="550" height="250" ></a> </div>
 	</center>
 
 	<?php
@@ -44,37 +44,85 @@
    			if($_SESSION['rol']== 'ADMINISTRADOR'){
    	 ?>
    	 	<hr style="color: #000000;" />
-    	<a href="altapropiedad.php"> Agregar propiedad </a>
+    </br>
+    <div align='center'>
+          	<a href="altapropiedad.php"> Agregar propiedad </a>
+      &nbsp|&nbsp
+      		<a href="solicitudPaseUsuario.php"> Ver solicitud de pases de usuarios </a>
+      &nbsp|&nbsp
+          	<a href="verClientes.php" > Ver Listado de Clientes </a>
+      &nbsp|&nbsp
+            <a href="modificarTarifas.php"> Ver tarifas </a>
+      &nbsp|&nbsp
+      	<?php } }
+      	if(isset($_SESSION['nombre'])){?>
 
-		<br/>
-		<a href="solicitudPaseUsuario.php"> Ver solicitud de pases de usuarios </a>
+      	<a href="listarSubastas.php"> Lista de subastas </a>
+      </br>
+      </br>
+      	<hr style="color: #000000;" />
+      <?php } ?>
+    </div>
+  <!-- Codigo del input buscar -->
+ <table align ='center'>
+    <tr>
+      <td>
+        <form name='buscarPropiedadPorNombre' action='index.php' method="GET" >
+          <fieldset><legend>Buscar por Nombre</legend>
+            <table>
+              <tr>
+                <td><input type= 'text' name='porNombre' id='porNombre' style="width: 100%"></td>
+                <td><button type="submit" > Buscar </button></td>
+              </tr>
+            </table>
+          </fieldset>
+       </form>
+     </td>
 
-    	<a href="verClientes.php" ><p style="text-align:center"> Ver Listado de Clientes </a>
+     <td>
+       <form name='buscarPropiedadPorCiudad' action='index.php' method="GET" >
+         <fieldset><legend>Buscar por Ciudad</legend>
+            <table>
+              <tr>
+                <td>
+                  <select name="porCiudad">
+                    <?php $ciudades ="SELECT ciudad FROM `propiedades`";
+                          $queryCiudades= mysqli_query($link,$ciudades);
+                          while ($fila=mysqli_fetch_array($queryCiudades)){ ?>
+                            <option><?php echo $fila['ciudad'] ?></option>
+                          <?php } ?>
+                </td>
+                <td><button type="submit" > Buscar </button></td>
+              </tr>
+            </table>
+         </fieldset>
+       </form>
+     </td>
 
-    	<br/>
-      <br/>
-      <a href="modificarTarifas.php"> Ver tarifas </a>
-      <br/>
-      <br/>
-	<?php } }
-	if(isset($_SESSION['nombre'])){?>
+    </tr>
+ </table>
+<!-- Fin Del input de busqueda -->
+  <?php
 
-	<a href="listarSubastas.php"><p style="text-align:left"> Lista de subastas </a>
+// COMIENZA EL CODIGO DE LA TABLA DE PROPIEDADES
 
-	<hr style="color: #000000;" />
-	<?php }
-//ACA COMIENZA EL CODIGO DE LA TABLA DE PROPIEDADES
-//orden
-if(!(isset($_GET['nombre']))&& !(isset($_GET['ciudad']))){//si no pidieron orden en particular
-   $query="SELECT foto, nombre, precio,pais, provincia,ciudad,id,estado FROM `propiedades` WHERE 1=1";//consulta a la base con solo datos q necesito
-}else {
-  if(isset($_GET['nombre'])){ // si se pidio orden por nombre
-    $query="SELECT * FROM `propiedades` ORDER BY `propiedades`.`nombre` ASC";
-  }else {
-    // si se pidio orden por ciudad
-      $query = "SELECT * FROM `propiedades` ORDER BY `propiedades`.`ciudad` ASC";
-  }
-}
+      $query="SELECT * FROM `propiedades`WHERE 1=1" ; // Colsulta sin condiciones
+
+      if(isset($_GET['porNombre'])){// en caso de que en se ingreso busqueda por nombre
+        $query.=' AND nombre LIKE "%'.$_GET['porNombre'].'%"';
+      }
+      if(isset($_GET['porCiudad'])){ //en caso de que en se ingreso busqueda por Ciudad
+        $query.=' AND ciudad LIKE "%'.$_GET['porCiudad'].'%"';
+      }
+      //orden
+      if((isset($_GET['nombre']))||(isset($_GET['ciudad']))){//si se realizo pedido de orden
+        if(isset($_GET['nombre'])){ // si se pidio orden por nombre
+          $query.=" ORDER BY `propiedades`.`nombre` ASC";
+        }else {
+          // si se pidio orden por ciudad
+            $query.= " ORDER BY `propiedades`.`ciudad` ASC";
+        }
+      }// fin del orden
 
      $consulta=mysqli_query($link, $query);
      if (mysqli_num_rows($consulta)>0) { ?> <!-- comparo el numero de filas que trajo la consulta con 0, si es 0 no trajo ningun dato-->
@@ -116,5 +164,10 @@ if(!(isset($_GET['nombre']))&& !(isset($_GET['ciudad']))){//si no pidieron orden
               	<?php } // Cierro el while?>
 					</table> <!-- fin de tabla -->
 				</center>
-	<?php } //cierro el if ?>
+	<?php }else{
+    if(isset($_GET['porNombre'])){
+      echo '<script> alert ("No existe propiedad con ese nombre");</script>';
+  		echo '<script> window.location="index.php"; </script>';
+    }
+  }?>
 </body>
