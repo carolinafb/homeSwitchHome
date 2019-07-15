dc33856c8936f7ddd00b0699eb457c613fd84776
carolina fb <carolinafernandezbeltran@gmail.com>
2019-07-14 22:59:48 -0300

    comprar hot+listar hot - cerrar hot

--- comprarHotsale.php
+++ comprarHotsale.php
@@ -1,1 +1,38 @@
-falta implementar
+
+<?php
+ 	include "conexion.php";
+	 $link=conexion();
+	 session_start();
+     $idPropiedad = $_GET['idProp'];
+  
+     $idHotsale= $_GET['idHot'];
+     echo "id h: ",$idHotsale,"id p: ",$idPropiedad;
+	
+
+	 if(isset($_SESSION['nombre'])){
+	 	
+            $query="UPDATE `hotsales` SET `estado`='CANCELADO' WHERE ID=$idHotsale";//consulta a la base con solo datos q necesito
+            $consulta=mysqli_query($link, $query);
+        
+            $queryHostale="SELECT * FROM hotsales WHERE ID=$idHotsale";
+            $consultaHotsale=mysqli_query($link, $queryHostale);
+            if (mysqli_num_rows($consultaHotsale) > 0) { 
+                $hotsale= mysqli_fetch_array($consultaHotsale);
+               
+                //esta cosa rara funca en el script de cerrar subastas pero aca no :(
+                $domingo = date("Y-m-d",strtotime(date($hotsale['lunes'])."+ 6 day"));
+                $lunes =date($hotsale['lunes']);
+                echo "FECHAS:   ",$lunes,"..",$domingo,"||",$hotsale['lunes']," ",$hotsale['domingo'];
+
+              
+                $queryReservar= "INSERT INTO `reservas`( `ID_propiedad`, `ID_usuario`, `fechaInicio`, `fechaFin`, `operacion`, `estado`, `semana`, `precio`) 
+                                                VALUES ($idPropiedad,{$_SESSION['id']},{$hotsale["lunes"]},{$hotsale["domingo"]},'HOTSALE','RESERVADO',{$hotsale["numeroSemana"]},{$hotsale["precio"]})";
+                $consulta=mysqli_query($link, $queryReservar);
+        }
+            //echo '<script> window.location="listarHotsalesVigentes.php"</script>';
+               //  $lunes =date($hotsale['lunes']);
+            //     $domingo = date("Y-m-d",strtotime(date($hotsale['lunes'])."+ 6 day"));
+               // echo "FECHAS:   ",$lunes,"..",$domingo,"||",$hotsale['lunes']," ",$hotsale['domingo'];
+} 
+?>
+  
--- listarHotsalesVigentes.php
+++ listarHotsalesVigentes.php
@@ -12,6 +12,26 @@ if (isset($_SESSION['nombre'])) {
 	  tr:nth-child(even) {background-color: #dddddd;}
 	  </style>
 	<div align = right><a href="cerrarSesion.php"> Cerrar sesion </a></div>
+
+	<script type="text/javascript">
+		function ConfirmDemo( idPropiedad, idHotsales) {
+		//Ingresamos un mensaje a mostrar
+		var mensaje = confirm("¿Desea confirmar la compra del hotsale ?");
+		//Detectamos si el usuario acepto el mensaje
+		if (mensaje) {
+			alert("¡Compra confirmada!");
+			document.location='comprarHotsale.php?idProp=' + idPropiedad + '&idHot=' + idHotsales;
+		}
+		//Detectamos si el usuario denegó el mensaje
+		else {
+		alert("¡Haz denegado la compra de hotsale!");  }
+	}
+	</script>
+
+
+
+
+
 </head>
 <body>
 		
@@ -22,7 +42,7 @@ if (isset($_SESSION['nombre'])) {
 						<h2>Hotsales </h2></center>
 		</caption><!-- titulo -->
 		<?php
-		$query = "SELECT propiedades.nombre, propiedades.foto, propiedades.ciudad, propiedades.pais, propiedades.provincia, propiedades.direccion,hotsales.ID as ID_hotsales, hotsales.estado, hotsales.precio, hotsales.ID_propiedad as ID_propiedad, hotsales.lunes FROM `propiedades` INNER JOIN hotsales ON propiedades.ID = hotsales.ID_propiedad where hotsales.estado='HOTSALE'"; //consulta a la base con solo datos q necesito
+		$query = "SELECT propiedades.ID as ID_propiedad ,propiedades.nombre, propiedades.foto, propiedades.ciudad, propiedades.pais, propiedades.provincia, propiedades.direccion,hotsales.ID as ID_hotsales, hotsales.estado, hotsales.precio, hotsales.ID_propiedad as ID_propiedad, hotsales.lunes FROM `propiedades` INNER JOIN hotsales ON propiedades.ID = hotsales.ID_propiedad where hotsales.estado='HOTSALE'"; //consulta a la base con solo datos q necesito
 		$consulta = mysqli_query($link, $query);
 		if ($fila=mysqli_num_rows($consulta) > 0) { ?>
 			<!-- comparo el numero de filas que trajo la consulta con 0, si es 0 no trajo ningun dato-->
@@ -60,19 +80,23 @@ if (isset($_SESSION['nombre'])) {
 									<?php echo $fila["precio"] ?>
 									<br /> Semana a subastar:
 									<?php echo $fila["lunes"] ?>
-									<br />
-
+									<br /> ID Hotsales:
+									<?php 
 									
+									$idProp=$fila["ID_propiedad"] ;
+									$idHot=$fila["ID_hotsales"] ;
+									echo $idHot ;?>
 								</td>
 								<!--cierro columna-->
 
 								<td>
 									<?php if ($_SESSION['rol'] == 'ADMINISTRADOR') { ?>
                                         <!--// aca va la linea de cerrar hotsale-->
-                                        <a href="cerrarHotsale.php"> Cerrar... </a>
+										<a href="aceptarHotsale.php"> Modificar... </a>
+										
 									<?php } else {
 									if ($_SESSION['rol'] == 'ESTANDAR' or $_SESSION['rol'] == 'PREMIUM') { ?>
-											<a href="comprarHotsale.php"> Comprar ... </a>
+											<input type="button" onclick="ConfirmDemo( <?php echo $idProp?>, <?php echo $idHot ?>)" value="Comprar" />;
 										<?php }
 								} ?>
 								</td>
